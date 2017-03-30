ca0829d783d4c585704b90660d44aa1cfc62a78d
David Utt <davidutt36@gmail.com>
2017-03-30 13:12:05 -0400

    Variable and Functions FrameWork

--- myCalc_DavidUtt/myCalc_DavidUtt/ViewController.swift
+++ myCalc_DavidUtt/myCalc_DavidUtt/ViewController.swift
@@ -15,11 +15,50 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBOutlet weak var myLable: UILabel!
+    
+    enum modes {
+        case not_set
+        case addition
+        case subbtraction
+    }
 
+    var savedNum = 0;
+    var currentMode: modes = .not_set
+    var lastButtonWasMode = false
+    var labelString: String = "0"
+
+    @IBAction func didPressClear(_ sender: UIButton) {
+    
+    }
+    
+    @IBAction func didPressNumber(_ sender: UIButton) {
+    
+    }
+    
+    @IBAction func didPressPlus(_ sender: UIButton) {
+    
+    }
+    
+    @IBAction func didPressEquals(_ sender: UIButton) {
+    }
+    
+    func changeMode(newMode:modes){
+    
+    }
+    
+    func updateText(){
+    
+    }
+    
+    
+    
 
 }
 
